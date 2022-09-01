b8ea1723bff7913d757f302659417e0b5d6e78b0
August Wollter <august.wollter@gmail.com>
2022-09-01 11:53:18 +0200

    added THE conf.py for online analyisis

---
+++ online/conf.py
@@ -0,0 +1,31 @@
+import plotting.image
+import analysis.agipd
+import analysis.event
+from backend import add_record
+import numpy as np
+import h5py as h5
+import extra_geom
+
+state = {}
+state['Facility'] = 'EuXFEL'
+state['EventIsTrain'] = True 
+state['EuXFEL/SelModule'] = 0 
+state['EuXFEL/DataSource'] = 'tcp://max-display002.desy.de:44551'
+
+
+def onEvent(evt):
+
+    #det = evt['photonPixelDetectors']['AGIPD01'].data
+    #module = add_record(evt["analysis"], "analysis", "single", det[0,:,:])
+
+    geom = extra_geom.AGIPD_1MGeometry.from_crystfel_geom('/home/alfredo/p003046/usr/Shared/alfredo/xfel3046/geometr\
+y/agipd_2995_v04.geom')
+    assem, centre = geom.position_modules_fast(np.empty(geom.expected_data_shape))
+
+    #det_arr[module_numbers] = mods[:,ind]                                                                           
+    #assem = geom.position_modules_fast(det_arr)[0][::-1,::-1]                                                       
+    #assem[np.isnan(assem)] = -1
+    #brightest_hit = add_record(evt['analysis'], 'analysis', 'Random hit', assem)
+    #random_image = add_record(evt["analysis"], "analysis", "Random Image", module.data[10,0])                       
+    print(assem.shape)
+    #plotting.image.plotImage(assem, history=10)
