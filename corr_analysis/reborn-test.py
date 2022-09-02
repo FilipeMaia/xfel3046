63dee0dc39b683572752acf214bad7b565bc21b0
YellowSub17 <patrick_aust@hotmail.com>
2022-09-02 11:13:31 +0200

    added more corr utils from roberto in reborn

--- corr_analysis/reborn-test.py
+++ corr_analysis/reborn-test.py
@@ -5,6 +5,7 @@ from reborn import detector, data
 
 from reborn.external import crystfel
 from reborn.viewers.mplviews import view_pad_data
+from reborn.external.euxfel import EuXFELFrameGetter
 # geomfile = data.cspad_geom_file
 # pads = crystfel.geometry_file_to_pad_geometry_list(geomfile)
 
@@ -12,8 +13,18 @@ from reborn.viewers.mplviews import view_pad_data
 # dat = pads.solid_angles()
 
 
+prop_num = 3046
 
-geom = reborn.external.crystfel.load_crystfel_geometry('./p3046_manual_refined_geoass_run10.geom')
+# geom = reborn.external.crystfel.load_crystfel_geometry('./p3046_manual_refined_geoass_run10.geom')
+# geom = reborn.external.crystfel.load_crystfel_geometry('/gpfs/exfel/exp/SPB/202202/p003046/scratch/rkirian/xfel3046/geometry/p3046_manual_refined_geoass_run10.geom')
+# geom = reborn.external.crystfel.load_crystfel_geometry('/gpfs/exfel/exp/SPB/202202/p003046/scratch/rkirian/xfel3046/geometry/p3046_manual_refined_geoass_run10.geom')
+# geom = reborn.external.crystfel.load_crystfel_geometry('./august.geom')
+
+geom = crystfel.geometry_file_to_pad_geometry_list('/gpfs/exfel/exp/SPB/202202/p003046/scratch/rkirian/xfel3046/geometry/p3046_manual_refined_geoass_run10.geom')
+fg = EuXFELFrameGetter(experiment_id=prop_num, run_id = 48, geom = geom)
+
+fg.view()
+
 
 
 print(geom)
